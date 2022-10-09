5e789c942104f8bb8e64cd9d4d8cc1d23ce2fd0a
Charles Cabergs <me@cacharle.xyz>
2022-10-09 18:33:42 +0200

    Added nvim treesitter go

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -352,6 +352,7 @@ return require("packer").startup(function()
                     "vim",
                     "yaml",
                     "meson",
+                    "go",
                 },
                 highlight = {
                     enable = true
