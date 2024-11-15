d73f7e2c84bd6dcb107691055acc5bf68b5b733f
Charles Cabergs <me@cacharle.xyz>
2024-11-15 13:18:48 +0100

    Add cowsay-total-war-quotes script

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -239,6 +239,8 @@ if status is-interactive
     if set -q VIRTUAL_ENV
         echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
     end
+
+    command -q cowsay-total-war-quotes && cowsay-total-war-quotes
 end
 
 if status is-login
