220b98c23806dc1d4153a78a785de5cdbd1a47d9
Charles Cabergs <me@cacharle.xyz>
2025-03-09 10:49:23 +0100

    Update yazi config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -151,7 +151,7 @@ vim.api.nvim_create_autocmd(
 )
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "cpp,c",
+    pattern = "cpp,c,cuda",
     callback = function()
         vim.bo.commentstring = "// %s"
     end,
--- config/yazi/plugins/smart-enter.yazi/init.lua
+++ config/yazi/plugins/smart-enter.yazi/init.lua
@@ -1,9 +1,12 @@
--- from: https://yazi-rs.github.io/docs/tips#smart-enter
-
+--- @since 25.2.26
 --- @sync entry
-return {
-	entry = function()
-		local h = cx.active.current.hovered
-		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-	end,
-}
+
+-- from: https://yazi-rs.github.io/docs/tips#smart-enter
+local function setup(self, opts) self.open_multi = opts.open_multi end
+
+local function entry(self)
+	local h = cx.active.current.hovered
+	ya.mgr_emit(h and h.cha.is_dir and "enter" or "open", { hovered = not self.open_multi })
+end
+
+return { entry = entry, setup = setup }
