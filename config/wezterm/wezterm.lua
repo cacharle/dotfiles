4ca4c0abc832bce230ab103926c481c626d70801
Charles Cabergs <me@cacharle.xyz>
2025-08-09 11:28:41 +0200

    Fix wezterm resize properly

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -9,6 +9,7 @@ config.enable_tab_bar = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
 config.window_close_confirmation = "NeverPrompt"
 config.warn_about_missing_glyphs = false
+config.adjust_window_size_when_changing_font_size = false
 
 config.keys = {
     { key = "K", mods = "ALT", action = act.ScrollByLine(-10) },
